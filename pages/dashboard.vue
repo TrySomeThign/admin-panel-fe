f9cb4cb30fb1ad6dcc39ebbef711d592cd3abcd0
conian <conian2505@gmail.com>
2024-02-25 19:31:29 +0700

    create page dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <Welcome />
+    <Statistic />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
